59031ef33693b9cc8231cb6c1f4fa0ae50f6fa41
vijaytemgire <vijay7455@gmail.com>
2017-12-13 15:24:47 +0530

    1st

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("errorCorrection.controller.View1", {
+	 onCollapseAll: function () {
+            var oTreeTable = this.getView().byId("TreeTableBasic");
+            oTreeTable.collapseAll();
+        },
+        onExpandFirstLevel: function () {
+            var oTreeTable = this.getView().byId("TreeTableBasic");
+            oTreeTable.expandToLevel(1);
+        }
+	});
+});
